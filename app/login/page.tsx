a5644bad17037cbbc81457533d86ae0f7664b344
DuckSave <ducluu1705@gmail.com>
2025-03-04 16:02:04 +0700

    new login

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ChatManament() { 
+    return (
+        <div>
+            heeeloooo Quỳnh Như , nhớ QN lắm á , ĐL nghiêm túc
+        </div>
+    )
+}
